d8b9b24d73bf6329248e81a310eda2c71daf7f5d
Martin D'Agostino <martin.dagostino@klarna.com>
2023-01-19 22:09:45 +0100

    wip

---
+++ api/jobs.go
@@ -0,0 +1,23 @@
+package nasa
+
+import (
+	"encoding/json"
+	"fmt"
+	"net/http"
+)
+
+func Jobs(w http.ResponseWriter, r *http.Request) {
+	w.WriteHeader(http.StatusCreated)
+	w.Header().Set("Content-Type", "application/json")
+	resp := make(map[string]string)
+	resp["message"] = "Hello World from Go"
+	resp["language"] = "go"
+	resp["cloud"] = "Hosted on Vercel!"
+	jsonResp, err := json.Marshal(resp)
+	if err != nil {
+		fmt.Println("Error happened in JSON marshal. Err: %s", err)
+	} else {
+		w.Write(jsonResp)
+	}
+	return
+}
