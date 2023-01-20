f50b132a0590fca8b6fa2fc21f938dfe0ea8c3d2
Martin D'Agostino <martin.dagostino@klarna.com>
2023-01-20 21:16:57 +0100

    move config

--- api/jobs.go
+++ api/jobs.go
@@ -14,6 +14,7 @@ import (
 )
 
 var configuration = config.GetConfig()
+var baseUrl = configuration["url"]
 
 func Jobs(w http.ResponseWriter, r *http.Request) {
 	w.Header().Set("Content-Type", "application/json")
@@ -31,7 +32,7 @@ func Jobs(w http.ResponseWriter, r *http.Request) {
 		}
 
 		index += factor
-		url := getUrl(r, index)
+		url := collector.GetUrl(r, baseUrl, index)
 
 		if common.IsError(err, "error calling upstream") {
 			w.WriteHeader(http.StatusInternalServerError)
--- collector/request.go
+++ collector/request.go
@@ -1,15 +1,13 @@
-package api
+package collector
 
 import (
 	"fmt"
-	"github.com/margostino/just/collector"
 	"github.com/margostino/just/common"
 	"net/http"
 )
 
-func getUrl(r *http.Request, index int) string {
+func GetUrl(r *http.Request, baseUrl string, index int) string {
 	params := getQueryParams(r)
-	baseUrl := configuration["url"]
 	pagination := fmt.Sprintf("position=1&pageNum=0&start=%d", index)
 	return fmt.Sprintf("%s?keywords=%s&location=%s&f_TPR=%s&%s", baseUrl, params.Keywords, params.Location, params.TimePeriod, pagination)
 }
@@ -18,11 +16,11 @@ func getQueryParam(r *http.Request, param string) string {
 	return common.NewString(r.URL.Query().Get(param)).ReplaceAll(" ", "%20").Value()
 }
 
-func getQueryParams(r *http.Request) *collector.QueryParams {
+func getQueryParams(r *http.Request) *QueryParams {
 	keywords := getQueryParam(r, "keywords")
 	location := getQueryParam(r, "location")
 	timePeriod := getQueryParam(r, "f_TPR")
-	return &collector.QueryParams{
+	return &QueryParams{
 		Keywords:   keywords,
 		Location:   location,
 		TimePeriod: timePeriod,
